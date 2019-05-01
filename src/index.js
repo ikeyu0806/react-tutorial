3ca73657e0ae3f97c259e65594f783b0f2a6eb0d
ikeyu0806 <ikeyu0806@gmail.com>
2019-05-02 08:41:01 +0900

    add constructor

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'x'})}>
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'x'})}
+      >
         {this.state.value}
       </button>
     );
