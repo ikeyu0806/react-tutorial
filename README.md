ff36ad2c2a2ca89019b3664adb3bca9955bebf9b
ikeyu0806 <ikeyu0806@gmail.com>
2019-05-04 17:54:52 +0900

    create seed file modify README

---
+++ README.md
@@ -0,0 +1,13 @@
+[reactのチュートリアル](https://ja.reactjs.org/tutorial/tutorial.html#before-we-start-the-tutorial) にRailsからAPI渡す
+
+React起動
+```
+
+npm start
+```
+
+Rails起動
+```
+
+bundle exec rails s -p 3001
+```
--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Comment.delete_all
+Comment.create!([
+  { author: 'サンプル１', text: 'sample01 book' },
+  { author: 'サンプル２', text: 'sample02 note' },
+  { author: 'サンプル３', text: 'sample03 ofuda' },
+])
